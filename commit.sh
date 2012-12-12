f747bf0abdee544568c91bff9c3c665650080c0a
Aidan Marlin <aidan.marlin@gmail.com>
2012-12-12 00:57:18 +0000

    Add C Check unit testing framework and make project play nice with autotools.

---
+++ commit.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+make clean
+if [[ $? != 0 ]]; then
+   exit
+fi
+
+autoreconf --install
+if [[ $? != 0 ]]; then
+   exit
+fi
+
+./configure
+if [[ $? != 0 ]]; then
+   exit
+fi
+
+make
+if [[ $? != 0 ]]; then
+   exit
+fi
+
+make check
+if [[ $? != 0 ]]; then
+   exit
+fi
+
+make distcheck
+if [[ $? != 0 ]]; then
+   exit
+fi
+
+mv gameboy-emulator-0.1.tar.gz dist/
+if [[ $? != 0 ]]; then
+   exit
+fi
+
+sudo make install
+if [[ $? != 0 ]]; then
+   exit
+fi
